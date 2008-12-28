40218e6ff4e23036a799f4db5489f0f47cc8a422
Chris Bratlien <chrisbratlien@gmail.com>
2008-12-28 15:41:35 +0800

    typo
    Signed-off-by: Gabriel Garrod  <gabrielg1976@gmail.com>

--- demo_cb2.rb
+++ demo_cb2.rb
@@ -19,15 +19,11 @@ end
 def test_drive(midi_object,root_note,scale,chord,duration = 1)
   chan=rand(8)
   chords = root_note.send(scale).all_harmonized_chords(chord)
-  #inverted until it's an octave up
-
   
   last_chord = chords.first
-  last_root_note = last_chord.root_note
-  
   #hacky
   last_chord = last_chord.invert
-  while last_chord.notes.first.name != last_root_note.name
+  while last_chord.notes.first.name != last_chord.root_note.name
     last_chord = last_chord.invert
   end
   
@@ -37,7 +33,7 @@ def test_drive(midi_object,root_note,scale,chord,duration = 1)
   #and finish on the last_chord
   play_chord(midi_object,last_chord,duration,chan,rand(75)+25)
 
-  #FIXME: not sure why the last_note seems to sustain for a longer time
+  #FIXME: not sure why the last_chord seems to sustain for a longer time
 end
 
 midi = LiveMIDI.new 
