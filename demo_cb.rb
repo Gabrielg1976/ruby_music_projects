5d86b4c905078a367320ec3c1794be0d83f938d4
Chris Bratlien <chrisbratlien@gmail.com>
2008-12-28 15:09:54 +0800

    play_chord function in demo_cb.rb
    Signed-off-by: Gabriel Garrod  <gabrielg1976@gmail.com>

--- demo_cb.rb
+++ demo_cb.rb
@@ -3,50 +3,32 @@ require 'scales'
 require 'rubygems'
 require 'cb-music-theory'
 
+def play_chord(midi_object,chord,channel,duration,velocity)
+  chord.note_values.each{|note|
+    midi_object.note_on(channel,note,velocity)
+  }
+  #sustain it a bit
+  sleep(duration)
+  #release the chord
+  chord.note_values.each{|note|
+    midi_object.note_off(channel,note,0)
+  }
+end
+
 
 midi = LiveMIDI.new 
  
-#a,b,d,e=rand(52)+33,rand(52)+33,rand(52)+33,rand(52)+33
-
-  # Channel / Note / Velocity
-#  puts "note" + " "+"#{count+=1}"
-
-#Note.new("C").major_chord.note_values.each{|note|
-#  midi.note_on(ch=rand(8),note,vlc=rand(75)+25)
-#  sleep(1)# = 1 second of time 60 = minute
-#  midi.note_off(ch,note,0)
-#}
-
-ch=rand(8)
+chan=rand(8)
 
 chords = Note.new("C").major_scale.all_harmonized_chords(:maj7_chord)
 
 #inverted until it's an octave up
 last_chord = chords.first.invert.invert.invert.invert
 
-chords.each{|c|
-  #play the chord
-  c.note_values.each{|note|
-    midi.note_on(ch,note,vlc=rand(75)+25)
-  }
-  #sustain it a bit
-  sleep(1)
-
-  #release the chord
-  c.note_values.each{|note|
-    midi.note_off(ch,note,0)
-  }
+chords.each{|chord|
+  play_chord(midi,chord,chan,1,rand(75)+25)
 }
-
 #and finish on the last_chord
-last_chord.note_values.each{|note|
-  midi.note_on(ch,note,vlc=rand(75)+25)
-}
-#sustain it a bit
-sleep(1)
-#release the chord
-last_chord.note_values.each{|note|
-  midi.note_off(ch,note,0)
-}
+play_chord(midi,last_chord,1,chan,rand(75)+25)
 
 puts "The Composition is Finished"
